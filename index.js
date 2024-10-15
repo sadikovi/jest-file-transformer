33214321053821bc7af01435237e7aa64a24c70d
Ivan Sadikov <ivan.sadikov@gmail.com>
2024-10-15 20:55:05 +1300

    update

---
+++ index.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const path = require("path");
+
+module.exports = {
+  process(sourceText, sourcePath, options) {
+    return {
+      code: `module.exports = ${JSON.stringify(sourceText)};`,
+    };
+  },
+};
